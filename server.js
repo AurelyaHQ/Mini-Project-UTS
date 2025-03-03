b4080976b415b3a3194986a06f904708c68ef280
AurelyaHQ <ayaya898@gmail.com>
2025-03-03 23:19:50 +0700

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors"); 
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 require("dotenv").config();
@@ -6,6 +7,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
